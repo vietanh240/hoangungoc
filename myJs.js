b9a7cc93208435d1ed1361f205054343f8ce9bac
wvietanhw <106524611+vietanh240@users.noreply.github.com>
2024-04-19 21:55:03 +0700

    Update myJs.js
    Signed-off-by: wvietanhw <106524611+vietanh240@users.noreply.github.com>

--- myJs.js
+++ myJs.js
@@ -144,7 +144,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "http://fb.com";
+            window.location = "https://vietanh240.github.io/hocngudot/";
           },
         });
       }
